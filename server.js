e40a2de9295a8fbeae1137f5ef0d9ea9d770ffda
GarethW1994 <williams.gareth0@gmail.com>
2017-04-07 15:26:53 +0200

    inital commit

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+
+//create a route
+//app.get('/', function (req, res) {
+//  res.send('Hello World!');
+//});
+
+//create hello codex route
+app.get('/hello', function(req, res) {
+    res.send('Hello Codex!');
+});
+//start server
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
